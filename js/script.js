632f6ebbda30705cd3af4229136a54a3d6b75ce9
Raquel <168922095+quel666@users.noreply.github.com>
2024-10-29 21:21:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nome = prompt ("Digite seu nome:")
+var idade =  String( prompt ("Digite a sua idade"))
+var curso = String ( prompt ( "Digite seu curso:"))
+var profissão= String ( prompt ( "Digite sua profissão:"))
+
+alert(`nome: ${nome}
+idade: ${idade}
+curso: ${curso}
+profissão ${profissão}
+`) 
